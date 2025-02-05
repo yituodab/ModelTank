14a1798a4758777438e2e7d800f425a36bc204d2
yituodab <a111664@qq.com>
2025-02-05 18:19:21 +0800

    update

--- src/main/java/com/model/tank/entities/tank/TankModel.java
+++ src/main/java/com/model/tank/entities/tank/TankModel.java
@@ -7,12 +7,12 @@ import software.bernie.geckolib.model.GeoModel;
 public class TankModel extends GeoModel<TankEntity> {
     @Override
     public ResourceLocation getModelResource(TankEntity tank) {
-        return tank.textureLocation != null ? tank.textureLocation : new ResourceLocation(ModularTank.MODID, "textures/texture.png");
+        return tank.modelLocation != null ? tank.modelLocation : new ResourceLocation(ModularTank.MODID, "geo/model.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(TankEntity tank) {
-        return tank.modelLocation != null ? tank.modelLocation : new ResourceLocation(ModularTank.MODID, "geo/model.geo.json");
+        return tank.textureLocation != null ? tank.textureLocation : new ResourceLocation(ModularTank.MODID, "textures/texture.png");
     }
 
     @Override
